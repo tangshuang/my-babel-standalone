0bab8ad97a5fc915ffa2337b6fc54aca1ba0110a
tisontang(唐霜) <tisontang@tencent.com>
2021-09-09 21:30:37 +0800

    update

--- build.js
+++ build.js
@@ -1,21 +1,24 @@
 const TerserPlugin = require('terser-webpack-plugin')
 const webpack = require('webpack')
+const fs = require('fs')
+const path = require('path')
+
 const webpackConfig = {
   mode: 'production',
   module: {
     rules: [
-      {
-        test: /\.js$/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            babelrc: false,
-            presets: [
-              '@babel/preset-env'
-            ]
-          }
-        }
-      }
+      // {
+      //   test: /\.js$/,
+      //   use: {
+      //     loader: 'babel-loader',
+      //     options: {
+      //       babelrc: false,
+      //       presets: [
+      //         '@babel/preset-env'
+      //       ]
+      //     }
+      //   }
+      // }
     ]
   },
   node: {
@@ -31,9 +34,12 @@ const webpackConfig = {
           }
         }
       })
-    ]
+    ],
+    usedExports: true,
+    sideEffects: true,
   },
   output: {
+    path: path.resolve(__dirname, 'dist'),
     library: 'Babel',
     libraryTarget: 'umd'
   },
@@ -53,20 +59,41 @@ const build = async (src, dst) => {
   const config = { ...webpackConfig, entry: `./src/${src}` }
   config.output.filename = dst
   if (dst.indexOf('.min.') === -1) {
-    config.mode = 'development'
+    config.mode = 'none'
     delete config.optimization
   }
   try {
     console.log(`pack( ${src}, ${dst} )`)
     const stats = await pack(config)
     console.log(stats.toJson('minimal'))
+    console.log('build success')
   }
   catch (e) {
     console.error(e)
   }
 }
 
+const cleanTaggedTemplateExpression = (file) => {
+  const filePath = path.resolve(__dirname, 'dist', file)
+  const content = fs.readFileSync(filePath).toString()
+  const code = content.replace(/\`([\w\W]+?)\`/gmi, (matched, innerCode) => {
+    if (innerCode.indexOf('\n') > -1) {
+      return '`\n' + innerCode.trim()
+        .replace(/\n+/g, '\n')
+        .replace(/ +/g, ' ')
+        .replace(/\n /g, '\n')
+        .replace(/\/\*.*?\*\//g, '')
+        .replace(/\/\/.*?\n/g, '')
+        .replace(/\n/g, '')
+        + '\n`'
+    }
+    return matched
+  })
+  fs.writeFileSync(filePath, code)
+}
+
 ;(async () => {
   await build('index.js', 'babel.js')
   await build('index.js', 'babel.min.js')
+  cleanTaggedTemplateExpression('babel.min.js')
 })()
--- src/index.js
+++ src/index.js
@@ -1,65 +1,98 @@
-import '@babel/polyfill'
-import 'whatwg-fetch'
-import * as BabelStandalone from './babel-standalone'
-export * from './babel-standalone'
+import { version, buildExternalHelpers, transform as babelTransform, transformFromAst as babelTransformFromAst, parse, traverse, types } from '@babel/core'
 
-BabelStandalone.registerPresets({
-  'env': require('@babel/preset-env')
-})
+const { isArray = arg => Object.prototype.toString.call(arg) === '[object Array]' } = Array
 
-BabelStandalone.registerPlugins({
-  'proposal-async-generator-functions': require('@babel/plugin-proposal-async-generator-functions'),
-  'proposal-class-properties': require('@babel/plugin-proposal-class-properties'),
-  'proposal-dynamic-import': require('@babel/plugin-proposal-dynamic-import'),
-  'proposal-function-bind': require('@babel/plugin-proposal-function-bind'),
-  'proposal-json-strings': require('@babel/plugin-proposal-json-strings'),
-  'proposal-nullish-coalescing-operator': require('@babel/plugin-proposal-nullish-coalescing-operator'),
-  'proposal-numeric-separator': require('@babel/plugin-proposal-numeric-separator'),
-  'proposal-object-rest-spread': require('@babel/plugin-proposal-object-rest-spread'),
-  'proposal-optional-catch-binding': require('@babel/plugin-proposal-optional-catch-binding'),
-  'proposal-optional-chaining': require('@babel/plugin-proposal-optional-chaining'),
-  'proposal-private-methods': require('@babel/plugin-proposal-private-methods'),
-  'proposal-unicode-property-regex': require('@babel/plugin-proposal-unicode-property-regex'),
-  'syntax-async-generators': require('@babel/plugin-syntax-async-generators'),
-  'syntax-class-properties': require('@babel/plugin-syntax-class-properties'),
-  'syntax-dynamic-import': require('@babel/plugin-syntax-dynamic-import'),
-  'syntax-json-strings': require('@babel/plugin-syntax-json-strings'),
-  'syntax-nullish-coalescing-operator': require('@babel/plugin-syntax-nullish-coalescing-operator'),
-  'syntax-numeric-separator': require('@babel/plugin-syntax-numeric-separator'),
-  'syntax-object-rest-spread': require('@babel/plugin-syntax-object-rest-spread'),
-  'syntax-optional-catch-binding': require('@babel/plugin-syntax-optional-catch-binding'),
-  'syntax-optional-chaining': require('@babel/plugin-syntax-optional-chaining'),
-  'syntax-top-level-await': require('@babel/plugin-syntax-top-level-await'),
-  'transform-arrow-functions': require('@babel/plugin-transform-arrow-functions'),
-  'transform-async-to-generator': require('@babel/plugin-transform-async-to-generator'),
-  'transform-block-scoped-functions': require('@babel/plugin-transform-block-scoped-functions'),
-  'transform-block-scoping': require('@babel/plugin-transform-block-scoping'),
-  'transform-classes': require('@babel/plugin-transform-classes'),
-  'transform-computed-properties': require('@babel/plugin-transform-computed-properties'),
-  'transform-destructuring': require('@babel/plugin-transform-destructuring'),
-  'transform-dotall-regex': require('@babel/plugin-transform-dotall-regex'),
-  'transform-duplicate-keys': require('@babel/plugin-transform-duplicate-keys'),
-  'transform-exponentiation-operator': require('@babel/plugin-transform-exponentiation-operator'),
-  'transform-for-of': require('@babel/plugin-transform-for-of'),
-  'transform-function-name': require('@babel/plugin-transform-function-name'),
-  'transform-literals': require('@babel/plugin-transform-literals'),
-  'transform-member-expression-literals': require('@babel/plugin-transform-member-expression-literals'),
-  'transform-modules-amd': require('@babel/plugin-transform-modules-amd'),
-  'transform-modules-commonjs': require('@babel/plugin-transform-modules-commonjs'),
-  'transform-modules-systemjs': require('@babel/plugin-transform-modules-systemjs'),
-  'transform-modules-umd': require('@babel/plugin-transform-modules-umd'),
-  'transform-named-capturing-groups-regex': require('@babel/plugin-transform-named-capturing-groups-regex'),
-  'transform-new-target': require('@babel/plugin-transform-new-target'),
-  'transform-object-super': require('@babel/plugin-transform-object-super'),
-  'transform-parameters': require('@babel/plugin-transform-parameters'),
-  'transform-property-literals': require('@babel/plugin-transform-property-literals'),
-  'transform-regenerator': require('@babel/plugin-transform-regenerator'),
-  'transform-reserved-words': require('@babel/plugin-transform-reserved-words'),
-  'transform-shorthand-properties': require('@babel/plugin-transform-shorthand-properties'),
-  'transform-spread': require('@babel/plugin-transform-spread'),
-  'transform-sticky-regex': require('@babel/plugin-transform-sticky-regex'),
-  'transform-template-literals': require('@babel/plugin-transform-template-literals'),
-  'transform-typeof-symbol': require('@babel/plugin-transform-typeof-symbol'),
-  'transform-unicode-escapes': require('@babel/plugin-transform-unicode-escapes'),
-  'transform-unicode-regex': require('@babel/plugin-transform-unicode-regex')
-})
+const availablePlugins = {}
+const availablePresets = {}
+
+function loadBuiltin (builtinTable, name) {
+  if (isArray(name) && typeof name[0] === 'string') {
+    if (builtinTable.hasOwnProperty(name[0])) {
+      return [builtinTable[name[0]]].concat(name.slice(1))
+    }
+    return
+  }
+  else if (typeof name === 'string') {
+    return builtinTable[name]
+  }
+  return name
+}
+
+function processOptions (options) {
+
+  const presets = (options.presets || []).map(presetName => {
+    const preset = loadBuiltin(availablePresets, presetName)
+    if (preset) {
+      if (isArray(preset) && typeof preset[0] === 'object' &&
+        preset[0].hasOwnProperty('buildPreset')) {
+        preset[0] = { ...preset[0], buildPreset: preset[0].buildPreset }
+      }
+    }
+    else {
+      throw new Error(`Invalid preset specified in Babel options: "${presetName}"`)
+    }
+    return preset
+  })
+
+  const plugins = (options.plugins || []).map(pluginName => {
+    const plugin = loadBuiltin(availablePlugins, pluginName)
+    if (!plugin) {
+      throw new Error(`Invalid plugin specified in Babel options: "${pluginName}"`)
+    }
+    return plugin
+  })
+
+  return {
+    babelrc: false,
+    ...options,
+    presets,
+    plugins,
+  }
+
+}
+
+function registerPlugin (name, plugin) {
+  if (availablePlugins.hasOwnProperty(name)) {
+    console.warn(`A plugin named "${name}" is already registered, it will be overridden`)
+  }
+  availablePlugins[name] = plugin
+}
+
+function registerPlugins (newPlugins) {
+  Object.keys(newPlugins).forEach(name => registerPlugin(name, newPlugins[name]))
+}
+
+function registerPreset (name, preset) {
+  if (availablePresets.hasOwnProperty(name)) {
+    console.warn(`A preset named "${name}" is already registered, it will be overridden`)
+  }
+  availablePresets[name] = preset
+}
+
+function registerPresets (newPresets) {
+  Object.keys(newPresets).forEach(name => registerPreset(name, newPresets[name]))
+}
+
+function transform (code, options) {
+  return babelTransform(code, processOptions(options))
+}
+
+function transformFromAst (ast, code, options) {
+  return babelTransformFromAst(ast, code, processOptions(options))
+}
+
+export {
+  version,
+  parse,
+  traverse,
+  types,
+  availablePlugins,
+  availablePresets,
+  buildExternalHelpers,
+  registerPlugin,
+  registerPlugins,
+  registerPreset,
+  registerPresets,
+  transform,
+  transformFromAst,
+}
